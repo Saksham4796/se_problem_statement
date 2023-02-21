684b5d1ccb9e1ab57699002165e0cd5a8eb6fbd1
Saksham4796 <sakshamsahai4796@gmail.com>
2023-02-20 23:53:12 -0700

    commit

--- extract_data.py
+++ extract_data.py
@@ -2,21 +2,21 @@ import mysql.connector
 import redis
 
 # Connect to the MySQL database
-conn = mysql.connector.connect(user='username', password='password', host='hostname', database='database')
+conn = mysql.connector.connect(user='root', password='sak479619', host='localhost', database='sales_db')
 cursor = conn.cursor()
 
 # Connect to Redis
 redis_conn = redis.Redis(host='localhost', port=6379, db=0)
 
 # Extract sales data from MySQL using SQL queries
-query = "SELECT product_name, date, units_sold FROM sales"
+query = "SELECT Quantity_Ordered, Price_Each, Product_Line, Order_Date FROM sales_data"
 cursor.execute(query)
 sales_data = cursor.fetchall()
 
 # Store the extracted data in Redis
 for data in sales_data:
-    product_name, date, units_sold = data
-    key = f"{product_name}:{date}"
+    quantity, price, product, date = data
+    key = f"{product}:{date}:{price}"
     redis_conn.set(key, units_sold)
 
 # Allow the master node to retrieve data based on specific dates, product names, etc.
